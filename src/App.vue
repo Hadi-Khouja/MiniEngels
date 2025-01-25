cb00d9d0d7682f358de9d1d16effd3a9be995013
Hadi Khouja <Hadi-khouja@outlook.de>
2025-01-25 20:42:45 +0100

    SM and MD Navigation

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,33 @@
 
 <template>
   <v-app>
-    <v-app-bar :elevation="2">
+    <v-app-bar :elevation="2" :extension-height="smAndDown ? 0 : 48">
       <template #prepend>
-        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-app-bar-nav-icon class="d-md-none" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       </template>
+
       <v-app-bar-title>
         <v-img :src="logo" :max-height="60" :max-width="200" :position="'left'"></v-img>
       </v-app-bar-title>
+
       <template #append>
         <v-btn class="mr-2" icon="mdi-magnify" :variant="'text'"></v-btn>
       </template>
 
       <template #extension>
-        <v-tabs align-tabs="title" :color="'secondary'">
-          <v-tab :text="'Home'" :to="{ name: 'HOME' }"></v-tab>
-          <v-tab :text="'Research'" :to="{ name: 'RESEARCH' }"></v-tab>
-          <v-tab :text="'Publications'" :to="{ name: 'PUBLICATIONS' }"></v-tab>
-          <v-tab :text="'News'" :to="{ name: 'NEWS' }"></v-tab>
-          <v-tab :text="'Events'" :to="{ name: 'EVENTS' }"></v-tab>
-          <v-tab :text="'Career'" :to="{ name: 'CAREER' }"></v-tab>
-          <v-tab :text="'Training'" :to="{ name: 'TRAINING' }"></v-tab>
-        </v-tabs>
+        <div class="h-auto">
+          <v-tabs class="d-none d-sm-none d-md-flex" align-tabs="title" :color="'secondary'">
+            <v-tab v-for="item of routes" :text="item.title" :to="item.to"></v-tab>
+          </v-tabs>
+        </div>
       </template>
     </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" temporary>
+      <v-list>
+        <v-list-item v-for="item of routes" :title="item.title" :to="item.to"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
     <v-main>
       <RouterView></RouterView>
@@ -39,6 +43,21 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 import { RouterView } from 'vue-router';
 import logo from '@/assets/logo.png';
+import { useDisplay } from 'vuetify';
+
+const drawer = ref(false);
+const { smAndDown } = useDisplay();
+
+const routes = [
+  { title: 'Home', to: { name: 'HOME' } },
+  { title: 'Research', to: { name: 'RESEARCH' } },
+  { title: 'Publications', to: { name: 'PUBLICATIONS' } },
+  { title: 'News', to: { name: 'NEWS' } },
+  { title: 'Events', to: { name: 'EVENTS' } },
+  { title: 'Career', to: { name: 'CAREER' } },
+  { title: 'Training', to: { name: 'TRAINING' } },
+];
 </script>
