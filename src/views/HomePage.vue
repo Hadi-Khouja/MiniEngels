861233bdded91429a2b17c3ab08e614dfa6f9cca
Hadi Khouja <Hadi-khouja@outlook.de>
2025-07-08 14:41:30 +0200

    test oidp login

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -11,59 +11,88 @@
       cover
     ></v-carousel-item>
   </v-carousel>
-  <p class="mx-8 mb-8 text-start">
-    Lorem ipsum odor amet, consectetuer adipiscing elit. Nascetur ex ac penatibus finibus fames
-    primis neque quam urna. Adipiscing ornare parturient torquent; nulla ornare varius risus dui.
-    Justo suspendisse eu, consequat maecenas tristique duis nisl suspendisse risus. Lacinia libero
-    metus euismod pretium mus elementum ultrices. Torquent vivamus turpis tempor nec tempus molestie
-    enim tristique. Nascetur mollis bibendum, felis class facilisi praesent. Ultricies non morbi sem
-    lacinia natoque ipsum. Nunc arcu vitae maximus; viverra viverra ultricies amet. Ante rutrum diam
-    porta porta tincidunt odio aenean. Dui nostra efficitur commodo lobortis ridiculus molestie.
-    Egestas fames sollicitudin dignissim conubia scelerisque proin tellus nisi. Nulla ornare mauris
-    amet integer augue aptent dapibus euismod. Suscipit convallis eget rhoncus parturient accumsan
-    id maximus maecenas. Massa lacus dapibus praesent urna volutpat. Montes auctor inceptos nam in
-    imperdiet; condimentum ligula nostra. Efficitur tempor natoque nascetur a eget viverra sit
-    posuere. Nascetur magnis risus rhoncus ac; purus pellentesque convallis tellus? Proin mi magnis
-    aptent, pellentesque cubilia penatibus mattis. Lobortis penatibus sem vivamus id neque conubia
-    cras. Rhoncus morbi odio viverra et finibus senectus. Sapien sed dignissim duis tempus himenaeos
-    molestie mi gravida dolor. Ridiculus primis porta cursus metus potenti finibus cursus commodo.
-    Est netus congue vivamus class turpis. Ultricies ultrices elementum habitant per; sed integer
-    sit dignissim. Odio sollicitudin eleifend molestie fusce mollis, viverra elit. Habitasse sit
-    laoreet sollicitudin ante, dictum nisl. Vulputate mollis fames orci magna scelerisque taciti
-    neque. Integer posuere himenaeos fames tempus varius. Hac libero curae auctor maecenas cursus.
-    Lorem enim donec dictum magnis potenti primis ultricies molestie. Quam penatibus taciti rutrum;
-    dis torquent eleifend. Ut lacus sagittis platea maximus tristique quisque nec dictumst. Pharetra
-    vitae nostra sem nostra, ultricies curae. Vivamus gravida at tincidunt rhoncus amet etiam
-    vivamus. Adipiscing dolor cursus quis ullamcorper; dignissim ultrices finibus. Nascetur litora
-    donec vehicula est class justo platea. Praesent posuere velit molestie aenean per. Pharetra
-    tempor nibh scelerisque netus faucibus. Senectus urna pretium aenean mauris taciti. Per accumsan
-    dictumst proin nibh tristique blandit sed. Libero vehicula mauris interdum fames hendrerit est.
-    Fames ex morbi porttitor pulvinar primis litora. Imperdiet urna lectus odio interdum velit eros
-    habitant. Etiam nascetur aliquet volutpat dictum ridiculus leo fames iaculis. Quisque curae
-    blandit commodo lobortis phasellus eros. Placerat mauris molestie nostra, fringilla sollicitudin
-    lorem netus purus. Mattis consectetur efficitur phasellus luctus facilisis quam nostra. Metus
-    sem elit cubilia phasellus netus aptent primis. Congue vitae nisl pellentesque varius finibus
-    vivamus. Litora proin sit varius felis blandit curabitur mi accumsan nam. Adolor faucibus
-    venenatis tempus feugiat ut justo posuere. Bibendum dignissim platea amet finibus vehicula
-    felis. Proin nostra neque a in ipsum nascetur. Himenaeos enim mollis ultricies ligula potenti.
-    Erat potenti habitant, amet donec metus mus. Adipiscing scelerisque nascetur nec senectus
-    efficitur vestibulum ultrices tortor. In fermentum libero placerat interdum massa senectus!
-    Fames congue tortor fusce fringilla ornare tristique metus ridiculus. Molestie sociosqu cras
-    euismod nec mattis commodo facilisis. Vestibulum vitae pellentesque lectus a hendrerit gravida.
-    Tincidunt eu porta parturient faucibus commodo primis est. Lorem porttitor pretium rhoncus
-    primis natoque donec arcu. Duis urna interdum sit nisl class. Venenatis accumsan hendrerit
-    aliquam diam sapien praesent urna. Nam est ipsum mus dolor facilisi diam. Sem hac erat ut
-    curabitur; sollicitudin venenatis. Non ac volutpat augue sed gravida massa montes. Vulputate
-    urna vivamus laoreet nam lacinia blandit. In nisi maximus hendrerit potenti donec non. Phasellus
-    mauris malesuada molestie pellentesque inceptos iaculis suspendisse id. Quam ac praesent dis
-    odio volutpat orci ligula aptent? Nam vestibulum sodales posuere lacus varius. Pretium lacus
-    lobortis aliquam placerat pretium quam velit. Pellentesque felis pulvinar; vehicula potenti
-    tincidunt taciti. Augue ridiculus sapien dapibus, fringilla vestibulum diam parturient placerat.
-    Suspendisse semper habitant elementum pretium ipsum turpis; nisl euismod nam. Fusce inceptos
-    enim nunc ac efficitur fusce elementum. Congue iaculis dui ornare felis euismod et fringilla
-    consequat. Eget curabitur condimentum vehicula ornare at iaculis massa integer metus. Molestie
-    nostra fringilla porttitor tempor risus dictumst varius.
-  </p>
+  <v-form id="form">
+        <v-text-field 
+            label="email"
+            v-model="email"
+            variant="filled"
+            id="user"
+            class="mb-3"
+            :autocomplete="'username'"
+            ref="usernamefield"
+            color="primary"
+            :readonly="userNameEntered"
+            type="email"
+            autofocus
+            name="username"
+        >
+          <template v-slot:append-inner>
+            <v-btn
+              v-if="!userNameEntered"
+              variant="plain"
+              icon='mdi-arrow-right'
+              @click="userNameEnteredHandler($event)"
+              id="enterUserName"
+              :loading="isProcessingUsername"
+            ></v-btn>
+            <v-btn 
+              v-else
+              id="changeUserName"
+              variant="text"
+              @click="userNameEntered=false; password=''"
+              color="primary"
+              :text="'change'"
+            ></v-btn>
+          </template>
+        </v-text-field>
+        <v-text-field 
+          v-show="userNameEntered"
+          :type="showPassword ? 'text' : 'password'"
+          :label="'password'"
+          placeholder=""
+          v-model="password"
+          variant="filled"
+          hide-details="auto"
+          :error-messages="errormessage"
+          id="password"
+          class="mb-2"
+          :autocomplete="'current-password'"
+          ref="passwordfield"
+          color="primary"
+          autofocus
+          name="password"
+        >
+          <template v-slot:append-inner>
+            <v-tooltip location="bottom" id="tooltipShowPassword">
+              <template v-slot:activator="{ props }">
+                <v-btn
+                  variant="plain"
+                  v-bind="props"
+                  id="pwdShowHideBtn"
+                  :aria-label="showPassword ? 'hide' : 'show'"
+                  :icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                  @click.stop="showPassword = !showPassword"
+                />
+              </template>
+              {{ showPassword ? 'hide' : 'show' }}
+            </v-tooltip>
+          </template>
+        </v-text-field>
+  </v-form>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const email = ref('');
+const password = ref('');
+const showPassword = ref(false);
+const userNameEntered = ref(false);
+const isProcessingUsername = ref(false);
+const errormessage = ref(''); 
+
+
+function userNameEnteredHandler(event: Event) {
+  userNameEntered.value = true;
+}
+</script>
